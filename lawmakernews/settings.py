7ade363a543489852ce4a39ec74a7173469082b6
qodot <sdaewon12@gmail.com>
2015-05-05 15:26:18 +0900

    change local db engine to postgreSQL

--- lawmakernews/settings.py
+++ lawmakernews/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 """
 Django settings for lawmakernews project.
 
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'lawmakernews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lawmakernews_local',
+        'USER': 'local_root',
+        'PASSWORD': '123123123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
