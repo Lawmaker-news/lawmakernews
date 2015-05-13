1bc8fca6a5d5afc56a7b1f824be52cae28cdc8db
sdw <sdaewon12@gmail.com>
2015-05-13 14:24:29 +0900

    add models Lawmaker Party Article

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=1024)
+    content = models.CharField(max_length=1024)
+    origin_created_at = models.DateTimeField()
+    origin_link = models.URLField(max_length=2048)
+    thumnail_link = models.URLField(max_length=2048)
+    press_company = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- lawmakers/models.py
+++ lawmakers/models.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from articles.models import Article
 
-# Create your models here.
+class Lawmaker(models.Model):
+    name = models.CharField(max_length=30)
+    party = models.ForeignKey('Party', related_name='lawmakers')
+    local = models.CharField(max_length=100)
+    generation = models.IntegerField()
+    is_current = models.BooleanField()
+    articles = models.ManyToManyField(Article, related_name='lawmakers')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Party(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
