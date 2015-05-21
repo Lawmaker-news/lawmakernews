db4306bd4e7a7c9b44f7e9dbfe6d6c7413617fc2
qodot <sdaewon12@gmail.com>
2015-05-22 00:06:32 +0900

    crawl all lawmakers

--- lawmakers/tasks.py
+++ lawmakers/tasks.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from celery import shared_task
 import requests
 from bs4 import BeautifulSoup
+import re
 
 @shared_task
 def crawl_all_lawmakers():
@@ -13,5 +14,12 @@ def crawl_all_lawmakers():
         crawl_each_lawmaker('http://www.rokps.or.kr/' + a_tag['href'])
 
 def crawl_each_lawmaker(url):
-    print url
-    print '@@@@@@@@@@@@@@'
+    response = requests.get(url)
+    table = BeautifulSoup(response.text).find(valign='top', align='left')
+    pattern = re.compile('성 명.{,20}>([가-힣]+).*제(19)대[[가-힣\s]*\((.{,20})\)([가-힣.\s]+)', re.DOTALL)
+    captures = pattern.search(unicode(table))    
+    print captures.group(1)
+    print captures.group(2)
+    print captures.group(3)
+    print captures.group(4)
+    print '-'*30
