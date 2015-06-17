1e8860766782defb8f5cfa8cc111c17587a30540
sdw <sdaewon12@gmail.com>
2015-06-17 14:21:44 +0900

    최근 24시간 이내의 기사만 가져오도록 수정

--- articles/tasks.py
+++ articles/tasks.py
@@ -19,29 +19,45 @@ def _crawl_articles_for(name):
             'query': '심상정',
             'ie': 'utf-8',
         })
+    
+    # 검색결과의 1페이지만 가져온다
     articles = BeautifulSoup(response.text).find(id='search_div').find_all('ul', recursive=False)
 
     for article in articles:
         ct_div = article.find('div', class_='ct')
 
         info_div = ct_div.find('div', class_='info')
-        date = info_div.find('span', class_='time').stripped_string
-        if re.match('[0-9]*일전', unicode(date)):
+        
+        # 최근 24시간 안의 기사만 가져온다
+        date = info_div.find('span', class_='time').string
+        is_after_day = re.match('.*[0-9]*일전.*', unicode(date))
+        is_after_week = re.match('.*[0-9]{4}.[0-9]{2}.[0-9]{2}.*', unicode(date))
+        if is_after_day or is_after_week:
             continue
+        
+        # 언론사
         press = info_div.find('span', class_='press').string
         
         title_a = ct_div.find('a', class_='tit')
+
+        # 원본 링크
+        origin_link = title_a['href']
+        
+        # 제목
         title = ''
         for string in title_a.stripped_strings:
             title += ' ' + string
-        origin_link = title_a['href']
         
         content_p = ct_div.find('p', class_='dsc')
+
+        # 간추린 내용
         content = ''
         for string in content_p.stripped_strings:
             content += ' ' + string
 
         thumbnail_a = article.find('a', class_='thmb')
+
+        # 썸네일 이미지 링크
         thumbnail_link = ''
         if thumbnail_a:
             thumbnail_link = thumbnail_a.find('img')['src']
