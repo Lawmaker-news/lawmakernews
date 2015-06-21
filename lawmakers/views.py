412649adaf596f7e4edd5d05a7f84b8de1e5acf7
qodot <sdaewon12@gmail.com>
2015-06-21 19:03:51 +0900

    모든 국회의원, 모든 정당 출력하는 API 작성

--- lawmakers/views.py
+++ lawmakers/views.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.forms.models import model_to_dict
+import json
+from lawmakers.models import Lawmaker, Party
 from lawmakers.tasks import crawl_all_lawmakers
+
+def lawmakers(request):
+    lawmakers = map(lambda lawmaker: model_to_dict(lawmaker, exclude='articles'), Lawmaker.objects.all())
+    
+    return HttpResponse(json.dumps(lawmakers), content_type='application/json')
+
+def parties(request):
+    parties = map(lambda party: model_to_dict(party), Party.objects.all())
+    
+    return HttpResponse(json.dumps(parties), content_type='application/json')
 
 def test_crawl_lawmakers(request):
     crawl_all_lawmakers.delay()
+    
     return render(request, 'temp.html')
