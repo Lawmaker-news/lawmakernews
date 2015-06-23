a883aeab80902cf75ea693b7517479591c401b65
sdw <sdaewon12@gmail.com>
2015-06-23 21:02:35 +0900

    기사 API 기본 구현

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from articles.tasks import crawl_all_articles, _crawl_articles_for
+from articles.models import Article
+
+def articles(request):
+    articles = map(lambda article: model_to_dict(article, exclude='lawmakers'), Article.objects.all())
+
+    return HttpResponse(json.dumps(articles), content_type='application/json')
 
 def test_crawl_articles(request):
     crawl_all_articles.delay()
