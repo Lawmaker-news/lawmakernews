c2b6616577ce8116713d327039a7ac270fbb2e1d
qodot <sdaewon12@gmail.com>
2015-09-13 16:01:43 +0900

    정당 목록 & 정당별 국회의원 목록 API

--- lawmakers/views.py
+++ lawmakers/views.py
@@ -7,15 +7,25 @@ import json
 from lawmakers.models import Lawmaker, Party
 from lawmakers.tasks import crawl_all_lawmakers, crawl_all_lawmakers_na
 
-def lawmakers(request):
-    lawmakers = map(lambda lawmaker: model_to_dict(lawmaker, exclude='articles'), Lawmaker.objects.all())
-    
-    return HttpResponse(json.dumps(lawmakers), content_type='application/json')
-
 def parties(request):
+    print 'parties'
+
     parties = map(lambda party: model_to_dict(party), Party.objects.all())
     
     return HttpResponse(json.dumps(parties), content_type='application/json')
+
+def party_lawmakers(request, id):
+    print 'party_lawmakers'
+
+    lawmakers = map(lambda lawmaker: model_to_dict(lawmaker, exclude='articles'), Lawmaker.objects.filter(party=id).all())
+
+    return HttpResponse(json.dumps(lawmakers), content_type='application/json')
+
+def all_party_lawmakers(request, id):
+    print 'all_party_lawmakers'
+    
+    return HttpResponse(json.dumps(lawmakers), content_type='application/json')
+
 
 def test_crawl_lawmakers(request):
     # crawl_all_lawmakers.delay()
